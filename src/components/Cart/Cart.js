f3c14a5f059016ef5b3a286136918fa3ff447ad7
konok101 <rockykonok@gmail.com>
2022-05-26 23:46:48 +0600

    calculation part done ema jhon er

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
+
+    let total=0;
+    let shipping= 0;
+    for(const prosuct of cart){
+        total=total+prosuct.price;
+        shipping= shipping+ prosuct.shipping;
+    }
+    const tax= total*10/100;
     return (
-        <div>
-             <h4>order summery</h4>
-                    <p>Selected item: {cart.length}</p>
+        <div className='cart'>
+            <h4>order summery</h4>
+            <p>Selected item: {cart.length}</p>
+            <p>Total price: {total}</p>
+            <p>Total Shipping: {shipping}</p>
+            <p>Tax:{tax} </p>
+            <h4>Grand Total: </h4>
         </div>
     );
 };
